8197c6ee8df942698a3954f9b4d7659a28486295
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-03-06 09:27:27 -0300

    first

---
+++ Ejercicio 1/hello_world/src/hello_world/First.java
@@ -0,0 +1,18 @@
+package hello_world;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		String name = "Jéssica";
+		int edad = 25;
+		String city = "Nueva Imperial";
+		
+		System.out.println("Mi nombre es: "+name);
+		System.out.println("\nTengo: "+edad+ " años de edad");
+		System.out.println("\nResido en la comuna de : "+city);
+		
+		
+	}
+
+}
