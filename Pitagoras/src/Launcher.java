b0c65c4b77e098523aa341df044121b726984fe3
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-03-06 09:49:33 -0300

    teorema pitágoras

---
+++ Pitagoras/src/Launcher.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Launcher {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingresa el número double de a: ");
+		double a = scanner.nextDouble();
+		System.out.println("Ingresa el número double de b: ");
+		double b = scanner.nextDouble();
+		
+		System.out.println("Hipotenusa para valores: \nA:"+a+"\nB:"+b);
+		
+		functionPitagoras(a , b);
+		
+	}
+	
+	public static double functionPitagoras(double a, double b) {
+		double result = 0;
+		
+		result = (double)Math.sqrt(a*a+b*b); 
+		System.out.println("Hipotenusa: "+result);
+		
+		return result;
+
+	}
+
+}
