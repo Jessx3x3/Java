c140377dd20a0fff3adeccd529f933b687f19b27
Jessx3x3 <j.vasquez12@ufromail.cl>
2020-03-16 13:12:00 -0300

    lista enlazada

---
+++ ListaEnlazada/src/SinglyLinkedList.java
@@ -0,0 +1,55 @@
+import java.util.Iterator;
+
+public class SinglyLinkedList {
+		
+		private int size;
+		public Node head;
+		
+	    public SinglyLinkedList() {
+	        this.size = 0;
+	        
+	    }
+	    
+	    public void add(int value) {
+	        Node newNode = new Node(value);
+	        this.size++;
+	        if(head == null) {
+	            head = newNode;
+	        } else {
+	            Node runner = head;
+	            while(runner.next != null) {
+	                runner = runner.next;
+	            }
+	            runner.next = newNode;
+	        }
+	    }    
+	    public void showNodeValue(Node node) {
+			
+	    	while(node.next != null) {
+	    		System.out.print(node.value+"-");
+	    		node = node.next;
+	    	}
+	    	if(node.next == null){
+	    		System.out.println(node.value);
+	    	}
+	    }
+	    public void printValues() {	
+	    	showNodeValue(this.head);
+	    }
+	    
+	    public void remove() {
+	    	
+	    	Node node = this.head;
+	    	Node aux = null;
+	    	while(node.next != null) {
+	    		aux = node;
+	    		node = node.next;
+	    	}
+	    	if(node.next == null){
+	    		aux.next = null;
+	    		System.out.println("\nRemoviendo el último valor: "+node.value+"\n");
+	    		node = null;
+	    	}
+	    }
+            
+}
